c04172f779cf91e85a242bb7a27dfb12ba0934eb
grant <abernathy.grant@icloud.com>
2025-10-17 11:39:52 -0500

    codestyle

---
+++ Code/_codestyle.gd
@@ -0,0 +1,27 @@
+#==================================================================================================
+# Copyright Grant Abernathy & Kendalynn Kohler. All rights reserved.
+#==================================================================================================
+# File:		code/_codestyle.gd
+# Author: 	Grant Abernathy & Kendalynn Kohler
+# Date:		October 17th, 2025
+#
+# Purpose:  Codestyle guidelines for the project.
+#==================================================================================================
+extends Node
+
+#
+# Codestyle Guidelines
+#
+# Always use 4 tabs for indentation.
+# Use snake_case for everything.
+#
+# Use f_ at the start of float variable names
+# Use i_ at the start of int variable names
+# Use b_ at the start of bool variable names
+# Use s_ at the start of string variable names
+# Use v2_ at the start of Vector2 variable names
+# Use v3_ at the start of Vector3 variable names
+# Use q_ at the start of Quaternion variable names
+# Use node_ at the start of Node references
+# Use func_ at the start of function names
+#
